acbfe89e2106ed7768190011494929c0870a6223
itssandeep-code <sandeepcdlu@gmail.com>
2024-08-28 16:17:27 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace learnazure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
